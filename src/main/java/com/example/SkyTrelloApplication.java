cdf13ba3e77889f9da492e51227c3b4dc70dfde7
skychb <skyler@nhnnext.org>
2016-06-21 15:25:43 +0900

    second

--- src/main/java/com/example/SkyTrelloApplication.java
+++ src/main/java/com/example/SkyTrelloApplication.java
@@ -2,8 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+import com.example.Thymeleaf3AutoConfiguration;
+
+@Import(Thymeleaf3AutoConfiguration.class)
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class SkyTrelloApplication {
 
 	public static void main(String[] args) {
