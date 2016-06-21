586457e4004948de650fc595fb0103f606c12dd9
skychb <skyler@nhnnext.org>
2016-06-21 13:27:17 +0900

    init

---
+++ src/main/java/com/example/SkyTrelloApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyTrelloApplication.class, args);
+	}
+}
